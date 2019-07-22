7ec89cfb9a21e6eb2467d746adb934f73b2542b1
John_Yang <jiahong9yang@126.com>
2019-07-22 12:50:26 +0800

    java9 interface

---
+++ src/main/java/interfaces/Interfaces.java
@@ -0,0 +1,37 @@
+package interfaces;
+
+import entity.Employee;
+
+import java.util.Comparator;
+
+public class Interfaces {
+    public static void main(String[] args) {
+        Employee mike = new Employee("Mike",2000);
+        Employee louise = new Employee("Louise",2500);
+
+        Comparator<Employee> byName = (a, b) -> a.getName().compareTo(b.getName());
+
+        System.out.println("By name:");
+        System.out.println(byName.compare(mike,louise));
+
+        try {
+            //throw NPE
+            System.out.println(byName.compare(mike, null));
+        }
+        catch ( NullPointerException e ){
+            System.out.println(e);
+        }
+
+        //a static method in comparator
+        Comparator<Employee> byNameThenNull = Comparator.nullsLast(byName);
+        System.out.println("Then null:");
+        System.out.println(byNameThenNull.compare(mike,louise));
+        System.out.println(byNameThenNull.compare(mike,null));
+
+        //a default method in comparator
+        Comparator<Employee> nullThenByDecreasingName = byNameThenNull.reversed();
+        System.out.println("Reversed");
+        System.out.println(nullThenByDecreasingName.compare(mike,louise));
+        System.out.println(nullThenByDecreasingName.compare(mike,null));
+    }
+}
