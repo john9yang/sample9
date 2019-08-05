daa822a46d5232616322ef89c4dc2c9627d5fe97
John_Yang <jiahong9yang@126.com>
2019-08-05 12:59:49 +0800

    java9 collisions

--- src/main/java/collisions/Time.java
+++ src/main/java/collisions/Time.java
@@ -6,13 +6,18 @@ import java.util.function.Supplier;
 public class Time {
 
     public static void timeIt(Runnable r){
-         timeIt(() -> {r.run();return 0;});
+         timeIt(
+                 () -> {r.run();
+                       return 0;
+                      }
+               );
     }
 
     public static <S,T> T timeIt(Function<S,T> fun,S arg){
         long start =  System.nanoTime();
         T result = fun.apply(arg);
         long stop = System.nanoTime();
+        System.out.println("result:"+result);
         System.out.println("Time: "+ (stop-start)/1000000.0+" msec");
         return result;
     }
